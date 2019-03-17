4be6ef037a6aee0529d8131c3c7cb59c7baa859f
ToastV4 <47010852+ToastV4@users.noreply.github.com>
2019-03-17 20:02:17 +0000

    Add files via upload

---
+++ Python Skyrim Pixel Art.py
@@ -0,0 +1,253 @@
+import turtle
+
+
+t = turtle.Turtle()
+t.width(1)
+t.penup()
+t.setposition(0,150)
+t.speed(1134)
+def square():
+  t.begin_fill()
+  for i in range(4):
+    t.forward(10)
+    t.right(90)
+  t.end_fill()
+
+for c in ["black","black","black"]:
+ t.color(c)
+ square()
+ t.forward(10)
+ 
+t.penup()
+t.right(90)
+t.forward(10)
+t.right(90)
+t.forward(50)
+t.left(180)
+t.pendown()
+
+for c in ["black","black","black","grey","black","black","black","black"]:
+ t.color(c)
+ square()
+ t.forward(10)
+
+t.right(90)
+t.forward(10)
+t.right(90)
+t.forward(90)
+t.left(180)
+
+for c in ["black","lightgrey","lightgrey","lightgrey","grey","lightgrey","lightgrey","lightgrey","lightgrey","black"]:
+ t.color(c)
+ square()
+ t.forward(10)
+
+t.penup()
+t.right(90)
+t.forward(10)
+t.right(90)
+t.forward(120)
+t.left(180)
+t.pendown()
+
+for c in ["black","black","lightgrey","lightgrey","lightgrey","lightgrey","grey","lightgrey","lightgrey","lightgrey","lightgrey","lightgrey","black","black"]:
+ t.color(c)
+ square()
+ t.forward(10)
+ 
+t.penup()
+t.right(90)
+t.forward(10)
+t.right(90)
+t.forward(150)
+t.left(180)
+t.pendown()
+
+for c in ["black","#ffb20d","black","lightgrey","grey","grey","grey","grey","grey","grey","grey","lightgrey","lightgrey","black","#ffb20d","black"]:
+ t.color(c)
+ square()
+ t.forward(10)
+
+ t.penup()
+t.right(90)
+t.forward(10)
+t.right(90)
+t.forward(170)
+t.left(180)
+t.pendown()
+
+for c in ["black","#ffb20d","#ffb20d","black","grey","#ffba8f","black","#ffba8f","grey","#ffba8f","black","#ffba8f","grey","lightgrey","black","#ffb20d","#ffb20d","black"]:
+ t.color(c)
+ square()
+ t.forward(10)
+
+t.penup()
+t.right(90)
+t.forward(10)
+t.right(90)
+t.forward(180)
+t.left(180)
+t.pendown()
+
+for c in ["black","#ffb20d","black","black","lightgrey","grey","#ffba8f","grey","grey","grey","#ffba8f","grey","lightgrey","lightgrey","black","black","#ffb20d","black"]:
+ t.color(c)
+ square()
+ t.forward(10)
+
+t.penup()
+t.right(90)
+t.forward(10)
+t.right(90)
+t.forward(180)
+t.left(180)
+t.pendown()
+
+for c in ["black","black","white","black","lightgrey","lightgrey","grey","grey","#ffba8f","grey","grey","lightgrey","lightgrey","lightgrey","black","white","black","black"]:
+ t.color(c)
+ square()
+ t.forward(10)
+
+t.penup()
+t.right(90)
+t.forward(10)
+t.right(90)
+t.forward(180)
+t.left(180)
+t.pendown()
+
+for c in ["black","white","white","black","lightgrey","grey","#ffba8f","black","black","black","#ffba8f","grey","lightgrey","grey","black","white","white","black"]:
+ t.color(c)
+ square()
+ t.forward(10)
+
+t.penup()
+t.right(90)
+t.forward(10)
+t.right(90)
+t.forward(150)
+t.left(180)
+t.pendown()
+
+for c in ["black","black","grey","#ffba8f","#ffba8f","#ffba8f","#ffba8f","#ffba8f","grey","grey","black","black"]:
+ t.color(c)
+ square()
+ t.forward(10)
+
+t.penup()
+t.right(90)
+t.forward(10)
+t.right(90)
+t.forward(100)
+t.left(180)
+t.pendown()
+
+for c in ["black","black","black","black","black","black","black","black","black","black"]:
+ t.color(c)
+ square()
+ t.forward(10)
+
+t.penup()
+t.right(90)
+t.forward(10)
+t.right(90)
+t.forward(110)
+t.left(180)
+t.pendown()
+
+for c in ["black","#ffba8f","#7a4827","#ffba8f","#ffba8f","#ffba8f","#7a4827","black","grey","grey","lightgrey","black"]:
+ t.color(c)
+ square()
+ t.forward(10)
+
+t.penup()
+t.right(90)
+t.forward(10)
+t.right(90)
+t.forward(140)
+t.left(180)
+t.pendown()
+
+for c in ["black","black","#ffba8f","black","#7a4827","#7a4827","#7a4827","#7a4827","black","grey","#7a4827","#7a4827","#7a4827","lightgrey","black"]:
+ t.color(c)
+ square()
+ t.forward(10)
+
+t.penup()
+t.right(90)
+t.forward(10)
+t.right(90)
+t.forward(190)
+t.left(180)
+t.pendown()
+
+for c in ["black","black","black","black","grey","black","#ffba8f","black","#7a4827","#7a4827","lightgrey","#7a4827","black","grey","#7a4827","lightgrey","#7a4827","lightgrey","black"]:
+ t.color(c)
+ square()
+ t.forward(10)
+
+t.penup()
+t.right(90)
+t.forward(10)
+t.right(90)
+t.forward(190)
+t.left(180)
+t.pendown()
+
+for c in ["black","lightgrey","lightgrey","lightgrey","grey","black","#7a4827","black","#7a4827","#7a4827","#7a4827","#7a4827","black","lightgrey","#7a4827","#7a4827","#7a4827","lightgrey","black"]:
+ t.color(c)
+ square()
+ t.forward(10)
+
+t.penup()
+t.right(90)
+t.forward(10)
+t.right(90)
+t.forward(180)
+t.left(180)
+t.pendown()
+
+for c in ["black","black","black","grey","black","black","black","black","black","black","black","black","black","lightgrey","lightgrey","lightgrey","black"]:
+ t.color(c)
+ square()
+ t.forward(10)
+
+t.penup()
+t.right(90)
+t.forward(10)
+t.right(90)
+t.forward(110)
+t.left(180)
+t.pendown()
+
+for c in ["black","lightgrey","lightgrey","grey","grey","grey","lightgrey","black","black","black"]:
+ t.color(c)
+ square()
+ t.forward(10)
+
+t.penup()
+t.right(90)
+t.forward(10)
+t.right(90)
+t.forward(100)
+t.left(180)
+t.pendown()
+
+for c in ["black","#7a4827","#7a4827","black","black","#7a4827","#7a4827","black"]:
+ t.color(c)
+ square()
+ t.forward(10)
+
+t.penup()
+t.right(90)
+t.forward(10)
+t.right(90)
+t.forward(80)
+t.left(180)
+t.pendown()
+
+for c in ["black","black","black","white","white","black","black","black"]:
+ t.color(c)
+ square()
+ t.forward(10)
+
+
